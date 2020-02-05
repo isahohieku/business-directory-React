9a84ee625622b647655f205fad3f104826c2d778
Isah Ohieku <isahohieku@gmail.com>
2020-02-05 21:34:47 +0100

    production build

---
+++ build/precache-manifest.e15b04e7c07aad013efffef846797802.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950c38079aa26769210e80af923cb77a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4781e2b6037953a02484",
+    "url": "/static/css/2.d5831cfa.chunk.css"
+  },
+  {
+    "revision": "584e142a664baf2d2d19",
+    "url": "/static/css/main.92afb0c1.chunk.css"
+  },
+  {
+    "revision": "4781e2b6037953a02484",
+    "url": "/static/js/2.fda4893e.chunk.js"
+  },
+  {
+    "revision": "eadf7cdf3ab77bdac64d268433c6bbd2",
+    "url": "/static/js/2.fda4893e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "584e142a664baf2d2d19",
+    "url": "/static/js/main.7c2cdaeb.chunk.js"
+  },
+  {
+    "revision": "b2004af9114258f8ece1",
+    "url": "/static/js/runtime-main.c8cd7d06.js"
+  },
+  {
+    "revision": "cb09ee5fd9e77f6295ec17d307e7389c",
+    "url": "/static/media/business.cb09ee5f.jpg"
+  },
+  {
+    "revision": "5f337553b9149796ea3a9e21ab2ac67d",
+    "url": "/static/media/lion.5f337553.png"
+  }
+]);
